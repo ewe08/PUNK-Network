5aa1ce8412e4cf45966b19f42ad6f767128b2180
grigoriylogin <Wileere@yadnex.ru>
2024-11-04 16:21:20 +0300

    add db commection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,20 @@
-package app
+package main
+
+import (
+	"PUNK-Network/pkg/repository"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Инициализация базы данных
+	db := repository.GetDB()
+	defer db.Close()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	log.Println("Сервер запущен на http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,1 +1,24 @@
 package models
+
+type User struct {
+	ID        int64
+	Firstname string
+	Lastname  string
+	Username  string
+	Password  string
+	PhoneID   int64
+}
+
+type Product struct {
+	ID          int64
+	Title       string
+	Description string
+	Price       int64
+	Seller      int64
+}
+
+type Phone struct {
+	ID         int64
+	Number     string
+	IsValidate bool
+}
